b0c4ee21e3e37fe1ad4429e0665d4eed2193d565
Mir Yasin Ali <miryasin.miki@gmail.com>
2025-02-22 12:46:08 +0530

    secondary_datatypes: Introduce Disc.

---
+++ secondary_datatypes/Disc.py
@@ -0,0 +1,11 @@
+# Disc
+
+students={"Name":["Yasin","MD","xyz"],"Marks":[90,90,89]}
+name=students["Name"]
+marks=students["Marks"]
+print(name)
+print(marks)
+print(students)
+print(name[0],"  ",marks[0])
+print(name[1],"  ",marks[1])
+print(name[2],"  ",marks[2])
